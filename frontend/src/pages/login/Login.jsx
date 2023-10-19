ec926de19576e1703f0374929c4be167e51091bc
Marcos Siqueira <106640310+Msiqueira1@users.noreply.github.com>
2023-10-19 11:44:21 -0300

    Inclusão API

--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -4,21 +4,37 @@ import "./login.scss"
 const Login = () => {
   return (
     <div>
-      <Navbar />
-      <div className="login">
-        <div className="container">
-          <div className="title">
-            Login
-          </div>
-          <div>
-            <input type="text" name="username" id="username" placeholder="Usuário" className="input" />
-            <input type="password" name="password" id="password" placeholder="Senha" className="input"/>            
-          </div>
-          <div className="botaoEntrar">Entrar</div>
-        </div>
-
+      <div>
+        <Navbar />
       </div>
-
+      <form>
+        <div className="login">
+          <div className="container">
+            <div className="title">
+              Login
+            </div>
+            <div>
+              <input
+                type="text"
+                name="username"
+                id="username"
+                placeholder="Usuário"
+                className="input"
+              />
+              <input
+                type="password"
+                name="password"
+                id="password"
+                placeholder="Senha"
+                className="input"
+              />
+            </div>
+            <div className="botaoEntrar">
+              Entrar
+            </div>
+          </div>
+        </div>
+      </form>
     </div>
   )
 }
